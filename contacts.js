a38cf5d8692d46769630ea1843a41809f7c687dc
ViktorZvonar <viktorzvonar@yahoo.com>
2023-03-22 23:22:37 +0200

    works

--- contacts.js
+++ contacts.js
@@ -1,21 +1,39 @@
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((item) => item.id === contactId);
+
+  return contact ? contact : null;
 }
 
 function removeContact(contactId) {
   // ...твій код
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("welcome to Node");
+const operations = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await operations.listContacts();
+      console.log(contacts);
+      break;
+    case "readById":
+      const contact = await operations.getContactById(id);
+      console.log("readById", contact);
+      break;
+    case "add":
+      await operations.addContact(name, email, phone);
+      break;
+    // case "replace":
+    //   await fs.writeFile(filePath, data);
+    //   break;
+    //   deafault: console.log("Unknown action");
+  }
+};
+
+// invokeAction({ action: "list" });
+
+// invokeAction({
+//   action: "readById",
+//   id: "AeHIrLTr6JkxGE6SN-0Rw",
+// });
+
+invokeAction({
+  action: "add",
+  name: "wolf",
+  email: "dsj@js.com",
+  phone: "345678899",
+});
