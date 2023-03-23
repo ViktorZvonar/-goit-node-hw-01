014c015134224e3586b3901415b85f163b92026c
ViktorZvonar <viktorzvonar@yahoo.com>
2023-03-23 11:49:48 +0200

    allWork

--- contacts.js
+++ contacts.js
@@ -16,8 +16,15 @@ async function getContactById(contactId) {
   return contact ? contact : null;
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id == contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 }
 
 async function addContact(name, email, phone) {
@@ -30,10 +37,24 @@ async function addContact(name, email, phone) {
   const contacts = await listContacts();
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+async function updateContact(id, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id == id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
 }
 
 module.exports = {
   listContacts,
   getContactById,
+  removeContact,
   addContact,
+  updateContact,
 };
--- index.js
+++ index.js
@@ -6,30 +6,53 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       const contacts = await operations.listContacts();
       console.log(contacts);
       break;
-    case "readById":
+    case "getById":
       const contact = await operations.getContactById(id);
-      console.log("readById", contact);
+      console.log("getById", contact);
+      break;
+    case "remove":
+      const removedContact = await operations.removeContact(id);
+      console.log(removedContact);
       break;
     case "add":
       await operations.addContact(name, email, phone);
       break;
-    // case "replace":
-    //   await fs.writeFile(filePath, data);
-    //   break;
-    //   deafault: console.log("Unknown action");
+    case "update":
+      const updatedContact = await operations.updateContact(id, {
+        name,
+        email,
+        phone,
+      });
+      console.log(updatedContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
 // invokeAction({ action: "list" });
 
 // invokeAction({
-//   action: "readById",
+//   action: "getById",
 //   id: "AeHIrLTr6JkxGE6SN-0Rw",
 // });
 
+// invokeAction({
+//   action: "add",
+//   name: "rtedsaudjd!!!!f",
+//   email: "QQQewazQdsj@js.com",
+//   phone: "3drtts22222222tttttrr234445678899",
+// });
+
+// invokeAction({
+//   action: "update",
+//   id: "aawzPRY_aiYNl-wJXl6wn",
+//   name: "OOOOOOOOOO",
+//   email: "dsj@confdhrs.com",
+//   phone: "345678899",
+// });
+
 invokeAction({
-  action: "add",
-  name: "wolf",
-  email: "dsj@js.com",
-  phone: "345678899",
+  action: "remove",
+  id: "aawzPRY_aiYNl-wJXl6wn",
 });
